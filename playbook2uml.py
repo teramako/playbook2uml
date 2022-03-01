f2cc6e18285448017154dba62621ff5a2a4c8e8d
teramako <teramako@gmail.com>
2022-03-01 22:09:20 +0900

    refactoring: move ArgumentParser to main()

--- playbook2uml.py
+++ playbook2uml.py
@@ -352,15 +352,16 @@ class UMLStatePlaybook:
 
         yield '@enduml'
 
-def main(args:Namespace):
+def main():
     '''main'''
+    ap = ArgumentParser(description='Ansible-Playbook to PlantUML')
+    ap.add_argument('PLAYBOOK', help='playbook file')
+    ap.add_argument('-T', '--title', type=str, help='The title of the playbook')
+    args = ap.parse_args()
+
     umlplaybook = UMLStatePlaybook(args.PLAYBOOK, option=args)
     for line in umlplaybook.generate():
         print(line)
 
 if __name__ == '__main__':
-    ap = ArgumentParser(description='Ansible-Playbook to PlantUML')
-    ap.add_argument('PLAYBOOK', help='playbook file')
-    ap.add_argument('-T', '--title', type=str, help='The title of the playbook')
-    args = ap.parse_args()
-    main(args)
+    main()
