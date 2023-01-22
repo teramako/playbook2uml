1549aeddf43587db49b2d88aee05338c8c8abdfd
teramako <teramako@gmail.com>
2023-01-22 19:02:44 +0900

    refactoring

--- playbook2uml/logger.py
+++ playbook2uml/logger.py
@@ -1,17 +1,17 @@
 import logging
 import sys
 
-LEVEL = ['WARNING', 'INFO', 'DEBUG']
+LEVEL = ('WARNING', 'INFO', 'DEBUG')
 
-black   = '\033[0;30m'
-red     = '\033[0;31m'
-green   = '\033[0;32m'
-yellow  = '\033[0;33m'
-blue    = '\033[0;34m'
-magenta = '\033[0;35m'
-cyan    = '\033[0;36m'
-white   = '\033[0;37m'
-reset   = '\033[0;0m'
+black   = '\033[30m'
+red     = '\033[31m'
+green   = '\033[32m'
+yellow  = '\033[33m'
+blue    = '\033[34m'
+magenta = '\033[35m'
+cyan    = '\033[36m'
+white   = '\033[37m'
+reset   = '\033[0m'
 
 def getLogger(name: str, verbose: int = 0) -> logging.Logger:
     logger = logging.getLogger(name)
